9a892f2f4d347025abf5a2dd193ecd058467492b
Zoey <825299623@qq.com>
2020-08-07 14:50:00 +0800

    Create same-tree.cpp

---
+++ LeetCodeRegular/DailyLC/LC_00100/same-tree.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        if( p == nullptr && q == nullptr){
+            return true;
+        }
+        else if(p == nullptr || q ==  nullptr){
+            return false;
+        }
+        else if(p -> val != q -> val){
+            return false;
+        }
+        else{
+            return isSameTree(p -> left, q -> left) && isSameTree(p -> right, q -> right);
+        }
+    }
+};
+
+/*
+深度优先算法DFS
+*/
