c5211b9f2ec644ae692a3c9e372b1aad3aee40dc
Zoey <825299623@qq.com>
2021-07-05 21:19:53 +0800

    Create binary-search.py

---
+++ LeetCodeRegular/算法/算法入门/LC_00704/binary-search.py
@@ -0,0 +1,29 @@
+"""
+704. 二分查找
+给定一个 n 个元素有序的（升序）整型数组 nums 和一个目标值 target  ，写一个函数搜索 nums 中的 target，如果目标值存在返回下标，否则返回 -1。
+
+
+示例 1:
+
+输入: nums = [-1,0,3,5,9,12], target = 9
+输出: 4
+解释: 9 出现在 nums 中并且下标为 4
+示例 2:
+
+输入: nums = [-1,0,3,5,9,12], target = 2
+输出: -1
+解释: 2 不存在 nums 中因此返回 -1
+"""
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        n = len(nums)
+        left, right = 0, n
+        while left < right:
+            if nums[(left+right)//2] < target:
+                left = (left+right)//2 + 1
+            elif nums[(left+right)//2] > target:
+                right = (left+right)//2
+            else:
+                return (left+right)//2
+        return -1 
