c0bca2801b9b6fdd93a2e957ff84138bc461a4ba
Zoey <825299623@qq.com>
2021-07-28 19:03:32 +0800

    Create reverse-linked-list.py

---
+++ LeetCodeRegular/算法/算法入门/递归/LC_00206/reverse-linked-list.py
@@ -0,0 +1,37 @@
+"""
+206. 反转链表
+给你单链表的头节点 head ，请你反转链表，并返回反转后的链表。
+ 
+
+示例 1：
+
+
+输入：head = [1,2,3,4,5]
+输出：[5,4,3,2,1]
+示例 2：
+
+
+输入：head = [1,2]
+输出：[2,1]
+示例 3：
+
+输入：head = []
+输出：[]
+"""
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseList(self, head: ListNode) -> ListNode:
+        if head is None:
+            return head
+        pre, cur = None, head
+        while cur != None:
+            mid = cur.next
+            cur.next = pre
+            pre = cur
+            cur = mid
+        return pre
