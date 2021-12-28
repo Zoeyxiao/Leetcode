7bc4f4e673afdd599f3022eaab7bc88bccf7488c
Zoey <825299623@qq.com>
2021-12-28 10:21:05 +0800

    Create two-sum.py

---
+++ LeetCodeRegular/数据结构/数据结构入门/数组/LC_00001/two-sum.py
@@ -0,0 +1,19 @@
+'''
+1. 两数之和
+给定一个整数数组 nums 和一个整数目标值 target，请你在该数组中找出 和为目标值 target  的那 两个 整数，并返回它们的数组下标。
+
+你可以假设每种输入只会对应一个答案。但是，数组中同一个元素在答案里不能重复出现。
+
+你可以按任意顺序返回答案。
+'''
+ 
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hashtable = dict()
+        for i, num in enumerate(nums):
+            if target - num in hashtable:
+                return (hashtable[target-num], i)
+            hashtable[nums[i]] = i 
+        return []
+      
+ #重点在于哈希表的应用和enumerate()语句的使用，需多次熟练
