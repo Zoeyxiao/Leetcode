581f64c41f637b7f3a723a34e25dcac65e643075
Zoey <825299623@qq.com>
2022-01-03 11:38:42 +0800

    Create remove-duplicates-from-sorted-list.py

---
+++ LeetCodeRegular/数据结构/数据结构入门/链表/LC_00083/remove-duplicates-from-sorted-list.py
@@ -0,0 +1,33 @@
+'''
+83. 删除排序链表中的重复元素
+存在一个按升序排列的链表，给你这个链表的头节点 head ，请你删除所有重复的元素，使每个元素 只出现一次 。
+返回同样按升序排列的结果链表。
+
+示例 1：
+输入：head = [1,1,2]
+输出：[1,2]
+
+示例 2：
+输入：head = [1,1,2,3,3]
+输出：[1,2,3]
+'''
+
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def deleteDuplicates(self, head: ListNode) -> ListNode:
+        if head is None:
+            return head
+        cur = head
+        while cur.next:
+            if cur.val == cur.next.val:
+                cur.next = cur.next.next
+            else:
+                cur = cur.next
+        return head
+        
+#一次遍历即可，要注意的是，不能直接从原头节点进行删减，而要引入一个新的头节点
